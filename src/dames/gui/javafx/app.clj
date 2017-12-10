80cf63599e87e2629212cd4caa5847effbaaaa7b
Roland Clairquin <rolandcl@azrael.fontaine>
2017-12-10 11:17:10 +0100

    Affichage du fond de damier en JavaFX

--- src/dames/gui/javafx/app.clj
+++ src/dames/gui/javafx/app.clj
@@ -1,15 +1,32 @@
 (ns dames.gui.javafx.app
   (:gen-class :extends javafx.application.Application)
   (:import (javafx.application Application)
-           (javafx.scene Scene))
+           (javafx.scene Scene)
+           (javafx.scene.layout GridPane)
+           (javafx.scene.shape Rectangle)
+           (javafx.scene.paint Color))
   )
 
 (defn -main [& args]
   (Application/launch dames.gui.javafx.app args))
 
+
+(defn init-grid []
+  (let [grid-pane (GridPane.)]
+    (doseq [x (range 10) y (range 10)]
+      (.add grid-pane
+            (if (even? (+ x y))
+              (Rectangle. 40 40 Color/CORNSILK)
+              (Rectangle. 40 40 Color/DARKSLATEGRAY))
+            y x))
+    grid-pane))
+
 (defn -start [this stage]
-  (doto stage
-    (.setTitle "Dames (JavaFX GUI)")
-    (.show))
+  (let [grid (init-grid)
+        scene (Scene. grid 500 500)]
+    (doto stage
+      (.setTitle "Dames (JavaFX GUI)")
+      (.setScene scene)
+      (.show)))
   )
 
